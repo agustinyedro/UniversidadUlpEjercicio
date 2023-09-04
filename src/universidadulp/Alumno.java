c8b8c05ace294ef4e9a389747dfb603249dc5cf1
Paula Redondo <126362902+PRPaulaRedondo@users.noreply.github.com>
2023-09-04 17:26:31 -0300

    Initial commit

---
+++ src/universidadulp/Alumno.java
@@ -0,0 +1,89 @@
+
+package universidadulp;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import javax.swing.JOptionPane;
+
+
+public class Alumno {
+    
+    private int idAlumno;
+    private int dni;
+    private String apellido, nombre;
+    private LocalDate fechaNacimiento;
+    private boolean estado;
+
+    public Alumno(int dni, String apellido, String nombre, LocalDate fechaNacimiento, boolean estado) {
+        
+        this.dni = dni;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.estado = estado;
+    }
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    public void agregarAlumno(Alumno alumno ){
+        Conexion conexion = new Conexion("Universidadulp");
+        String sql= "INSERT INTO alumno (idAlumno,dni,apellido,nombre,fechaNacimiento,estado) "
+                + "VALUES(?,'"+alumno.getNombre()+"','"+alumno.getApellido()+"','"+alumno.getFechaNacimiento()+"',"+alumno.isEstado()+")";
+     
+        try {
+            PreparedStatement ps=conexion.conectar().prepareStatement(sql);            
+            ps.executeUpdate();
+         
+        } catch (SQLException ex) {
+           JOptionPane.showMessageDialog(null,"Error en la consulta");
+        }
+    }
+    
+}
---
+++ src/universidadulp/UniversidadUlp.java
@@ -0,0 +1,41 @@
+
+package universidadulp;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+
+public class UniversidadUlp {
+
+   
+    public static void main(String[] args) {
+        //con nombre de la base de datos
+        Conexion conexion = new Conexion("Universidadulp"); 
+        conexion.conectar();
+        
+        Alumno alumno1= new Alumno(36314380, "Redondo", "Paula", LocalDate.of(1992, Month.OCTOBER, 7), true);
+        alumno1.agregarAlumno(alumno1);
+        
+//        String sql= "INSERT INTO alumno VALUES (null, 36314380, 'Redondo', 'Paula', '1992-10-07',true)";
+//        
+//        PreparedStatement ps;
+//        try {
+//            ps = conexion.conectar().prepareStatement(sql);
+//            ps.executeQuery();
+//        } catch (SQLException ex) {
+//           JOptionPane.showMessageDialog(null,"Error en la consulta");
+//        }
+//        
+    
+   }
+      
+        
+        
+    }
+    
+
